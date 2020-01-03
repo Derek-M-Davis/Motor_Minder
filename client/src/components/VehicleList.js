36d027ab21b4e2445431951ec2d02dfa4e276c6b
Derek <derekdavis408@gmail.com>
2020-01-03 10:34:02 -0800

    works now...

--- client/src/components/VehicleList.js
+++ client/src/components/VehicleList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import uuid from '';
+import uuid from 'uuid';
 
 class VehicleList extends Component {
     state = {
@@ -10,7 +10,7 @@ class VehicleList extends Component {
             {id: uuid(), name: 'Honda'},
             {id: uuid(), name: 'Subaru'}, 
             {id: uuid(), name: 'Lexus'},
-            {id: uuid(), name: 'GMC'},
+            {id: uuid(), name: 'GMC'}
         ]
     }
 
@@ -21,16 +21,39 @@ class VehicleList extends Component {
                 <Button 
                     color="dark" 
                     style={{marginBottom: '2rem'}} 
-                    onClick={() => { const name = prompt('Enter Vehicle');
+                    onClick={() => { 
+                        const name = prompt('Enter Vehicle');
                         if(name) {
                             this.setState(state => ({
                                 vehicles: [...state.vehicles, {id: uuid(), name}]
                             }));
                         }
-                    }}>Add Vehicle
+                    }}>
+                    Add Vehicle
                 </Button>
+                <ListGroup>
+                    <TransitionGroup className="vehicle-list">
+                        {vehicles.map(({id, name})=>(
+                          <CSSTransition key={id} timeout={500} classNames="fade">
+                              <ListGroupItem>
+                                  <Button
+                                  className="remove-btn"
+                                  color="danger"
+                                  size="sm"
+                                  onClick={()=>{
+                                      this.setState(state => ({
+                                          vehicles: state.vehicles.filter(vehicle =>vehicle.id !== id)
+                                      }))
+                                  }}
+                                  >&times;</Button>
+                                  {name}
+                              </ListGroupItem>
+                          </CSSTransition>  
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
-        )
+        );
     }
 }
 
