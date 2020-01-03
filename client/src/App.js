16a3838a7dce479d3dd40d7fa3a078ec85c637cd
Derek <derekdavis408@gmail.com>
2020-01-03 13:42:52 -0800

    added modal and container

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import VehicleList from './components/VehicleList';
+import VehicleModal from './components/VehicleModal';
+import {Container} from 'reactstrap';
+
+import  {Provider} from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -8,10 +13,15 @@ import './App.css';
 class App extends Component {
   render(){
     return (
+      <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <VehicleList />
+        <Container>
+          <VehicleModal />
+          <VehicleList />
+        </Container>
       </div>
+      </Provider>
     )
   }
 }
