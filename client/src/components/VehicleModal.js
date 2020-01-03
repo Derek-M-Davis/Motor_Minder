9317288afa9264c39240d998aea543df7a5000e8
Derek <derekdavis408@gmail.com>
2020-01-03 13:44:07 -0800

    made vehicle modal

---
+++ client/src/components/VehicleModal.js
@@ -0,0 +1,83 @@
+import React, { Component} from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup} from 'reactstrap';
+import { connect} from 'react-redux';
+import { addVehicle} from '../actions/vehicleActions';
+import uuid from 'uuid';
+
+class VehicleModal extends Component {
+    state = {
+        modal: false,
+        name: ''
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    };
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const newVehicle = {
+            id: uuid(),
+            name: this.state.name
+        }
+        // Add Vehicle
+        this.props.addVehicle(newVehicle);
+        // Close Modal
+        this.toggle();
+    }
+
+    render(){
+        return (
+            <div>
+                <Button
+                    color="dark"
+                    style={{marginBottom: '2rem'}}
+                    onClick={this.toggle}
+                >
+                Add Vehicle
+                </Button>
+                <Modal
+                    isOpen={this.state.modal}
+                    toggle={this.toggle}
+                >
+                    <ModalHeader toggle={this.toggle}>
+                        Add to Vehicles
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.onSubmit}>
+                            <FormGroup>
+                                <Label for="vehicle">
+                                    Vehicle
+                                </Label>
+                                <Input
+                                type="text"
+                                name="name"
+                                id="vehicle"
+                                placeholder="Add Vehicle"
+                                onChange={this.onChange}
+                                />
+                                <Button
+                                    color="dark"
+                                    style={{marginTop: '2rem'}}
+                                    block
+                                >Add Vehicle</Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    vehicle: state.vehicle    
+});
+
+export default connect(mapStateToProps, {addVehicle})(VehicleModal);
