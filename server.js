9cd4ef86c2038350fb1486de8e7916d220345eb8
Derek <derekdavis408@gmail.com>
2020-01-03 13:42:08 -0800

    added in some comments and mongoose config

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // ----- Mongoose Config ----- //
-mongoose.connect(db)
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex: true })
     .then(()=> console.log('MongoDB connnected ...'))
     .catch(err=> console.log(err));
-
+// ----- Components Config ----- //
 app.use('/api/vehicles', vehicles);
 
+// ----- Listner  ----- //
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server started on port ${port}`));  
