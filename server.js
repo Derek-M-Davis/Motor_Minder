9e806e5a46968b2fc30e708e279881c2aa133e61
Derek <derekdavis408@gmail.com>
2020-01-03 14:34:29 -0800

    added production scripts

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const vehicles = require('./routes/api/vehicles');
 const app = express();
@@ -16,8 +17,18 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex: true })
     .then(()=> console.log('MongoDB connnected ...'))
     .catch(err=> console.log(err));
+
 // ----- Components Config ----- //
 app.use('/api/vehicles', vehicles);
+
+// Serve Static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
 
 // ----- Listner  ----- //
 const port = process.env.PORT || 5000;
