e708888860c695352883baa5b958c8ed1345672e
Derek <derekdavis408@gmail.com>
2020-01-04 13:35:21 -0800

    added user config

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 // ----- Dependencies ----- //
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('config');
 
-const vehicles = require('./routes/api/vehicles');
 const app = express();
 
 // ----- Middleware ----- //
-app.use(bodyParser.json()); 
+app.use(express.json()); 
 
 // ----- DB Config ----- //
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // ----- Mongoose Config ----- //
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex: true })
@@ -19,7 +18,9 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true, useFindAnd
     .catch(err=> console.log(err));
 
 // ----- Components Config ----- //
-app.use('/api/vehicles', vehicles);
+app.use('/api/vehicles', require('./routes/api/vehicles'));
+app.use('/api/users', require('./routes/api/users'));
+
 
 // Serve Static assets if in production
 if(process.env.NODE_ENV === 'production') {
