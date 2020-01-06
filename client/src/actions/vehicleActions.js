d794fcf0efd5ab3ebf180fb468a5d9b0a5afbe09
Derek <derekdavis408@gmail.com>
2020-01-05 22:39:12 -0800

    added token config to add and delete

--- client/src/actions/vehicleActions.js
+++ client/src/actions/vehicleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import {GET_VEHICLES, ADD_VEHICLE, DELETE_VEHICLE, VEHICLES_LOADING} from './types';
+import { tokenConfig} from './authActions';
+import { returnErrors} from './errorActions';
 
 export const getVehicles = () => dispatch => {
    dispatch(setVehiclesLoading());
@@ -9,28 +11,29 @@ export const getVehicles = () => dispatch => {
         dispatch({
             type: GET_VEHICLES,
             payload: res.data
-        })
-    )
+    }))
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-export const addVehicle = (vehicle) => dispatch => {
+export const addVehicle = (vehicle) => (dispatch, getState) => {
   axios
-  .post('/api/vehicles', vehicle)
+  .post('/api/vehicles', vehicle, tokenConfig(getState))
   .then(res => 
     dispatch({
         type: ADD_VEHICLE,
         payload: res.data
     }))
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-export const deleteVehicle = (id) => dispatch => {
+export const deleteVehicle = (id) => (dispatch, getState) => {
     axios
-    .delete(`/api/vehicles/${id}`)
+    .delete(`/api/vehicles/${id}`, tokenConfig(getState))
     .then(res => dispatch({
         type: DELETE_VEHICLE,
         payload: id
-        })
-    )
+    }))
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
 export const setVehiclesLoading = () => {
