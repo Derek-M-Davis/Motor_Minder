2a93f1a79013ff5ca84bd760dae5e31cc0fc3756
Derek <derekdavis408@gmail.com>
2020-01-05 21:54:23 -0800

    added register user and logout action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -15,7 +15,8 @@ import {
 export const loadUser = () => (dispatch, getState) => {
     dispatch({ type: USER_LOADING});
 
-    axios.get('/api/auth/user', tokenConfig(getState))
+    axios
+    .get('/api/auth/user', tokenConfig(getState))
     .then(res => dispatch({
         type: USER_LOADED,
         payload: res.data
@@ -27,6 +28,37 @@ export const loadUser = () => (dispatch, getState) => {
         });
     });
 };
+
+// Register User
+export const register = ({name, email, password}) => dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({name, email, password});
+
+    axios.post('/api/users', body, config)
+    .then(res => dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+        })
+    )
+    .catch (err => {
+        dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
+        dispatch({
+            type: REGISTER_FAIL
+        });
+    });
+} 
+
+// Logout User
+export const logout = () => {
+    return {
+        type: LOGOUT_SUCCESS
+    }
+}
 
 // Setup Config/Headers and Token
 export const tokenConfig = getState => {
