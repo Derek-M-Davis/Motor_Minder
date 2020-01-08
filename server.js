d60c86f04143259b33458b9baf13fcc1de6a4162
Derek <derekdavis408@gmail.com>
2020-01-07 18:34:34 -0800

    added events component

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true, useFindAnd
     .catch(err=> console.log(err));
 
 // ----- Components Config ----- //
-app.use('/api/vehicles', require('./routes/api/vehicles'));
+app.use('/api/users/vehicles', require('./routes/api/vehicles'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users/vehicles/events', require('./routes/api/events'));
+
 
 
 
